4794152d2ccc6c9f8a23e5861f3ce51a46be980f
Kyle Smith <kyle@handled.com>
2020-04-24 00:08:28 -0500

    Initial commit for Handled Start Page Chrome extension

---
+++ js/clock.js
@@ -0,0 +1,39 @@
+const months = ["January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"];
+
+window.addEventListener('load', getTime);
+window.addEventListener('load', getDate);
+
+function getTime(){
+    var date = new Date();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var session = "AM";
+
+    if(hour == 0){
+        hour = 12;
+    } else if(hour == 12){
+        hour = 12;
+        session = "PM";
+    } else if(hour > 12){
+        hour = hour - 12;
+        session = "PM";
+    }
+
+    minute = (minute < 10) ? "0" + minute : minute;
+
+    var time = hour + ":" + minute + " " + session;
+    document.getElementById("clock").innerText = time;
+
+    setTimeout(getTime, 1000);
+}
+
+function getDate(){
+    var date = new Date();
+    var month = date.getMonth();
+    var day = date.getDate();
+    var year = date.getFullYear();
+
+    var currentDate = months[month] + " " + day + ", " + year;
+    document.getElementById("date").innerText = currentDate;
+}
---
+++ js/todo.js
@@ -0,0 +1,85 @@
+var list;
+var form;
+var task;
+const notifications = ["Added Task", "Deleted Task", "Updated To Do List", "An error occurred"];
+
+window.addEventListener('load', toDoList);
+
+function toDoList(){
+  this.list = document.querySelector('#toDoList');
+  this.form = document.querySelector('#addToDoForm');
+  this.task = document.querySelector('#addTaskInput');
+
+  form.addEventListener('submit',function( event ){
+    // Prevents the form submission from refreshing the page.
+    event.preventDefault();
+
+    // If the list hasn't been saved in localstorage before,
+    // remove the default info message
+    if (!window.localStorage.startPageToDoList){
+      list.innerHTML = null;
+    }
+
+    list.innerHTML += '<li class="mdl-list__item">' + task.value
+        + '<div class="removeTask"><a class="removeTaskLink"><i class="material-icons mdl-list__item-icon">delete</i></a></div></li>';
+
+    storeList(notifications[0]);
+
+    task.value = '';
+  },false)
+
+  $(document).on('click', 'ul#toDoList li', function() {
+    var toDoTask = event.target;
+
+    // Don't trigger the completion event for the default message
+    if (!toDoTask.classList.contains('default')){
+      //Mark the task as completed, or undo marking as completed
+      if(!toDoTask.classList.contains('completed')){
+        toDoTask.classList.add('completed');
+      } else if(toDoTask.classList.contains('completed')){
+        toDoTask.classList.remove('completed');
+      }
+      //After updating the task's completion status, store the list
+      storeList(notifications[2]);
+    }
+  });
+
+  //Get the To Do list from the browser's local storage
+  function getValues() {
+    var storedValues = window.localStorage.startPageToDoList;
+    if(!storedValues) {
+      return;
+    }
+    else {
+      list.innerHTML = storedValues;
+    }
+  }
+
+  // Due to Chrome plugin security policy, regular 'onclick' events are not allowed, so jQuery must
+  // be implemented in order to remove tasks from the To Do list
+  $(document).on('click', 'ul#toDoList li a.removeTaskLink', function( event ) {
+    $(this).closest('li').remove();
+    storeList(notifications[1]);
+    event.stopImmediatePropagation();
+  });
+
+  getValues();
+}
+
+// Write the To Do list to the browser's local storage
+function storeList(messageText) {
+  try {
+    window.localStorage.startPageToDoList = list.innerHTML;
+  } catch(err) {
+    console.log("Error saving To Do list to localStorage");
+    notify(notifications[3]);
+  } finally {
+    notify(messageText);
+  }
+}
+
+function notify(messageText) {
+  var snackbarContainer = document.querySelector('#notificationSnackbar');
+  var data = {message: messageText};
+  snackbarContainer.MaterialSnackbar.showSnackbar(data);
+}
