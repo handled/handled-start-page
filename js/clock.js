3755586d7d83c6fab71fae0fddf6fbf15ae5ddc9
Kyle Smith <kyle@handled.com>
2020-09-09 15:37:53 -0500

    Add more links, update formatting

--- js/clock.js
+++ js/clock.js
@@ -1,21 +1,21 @@
-const months = ["January", "February", "March", "April", "May", "June",
-                "July", "August", "September", "October", "November", "December"];
+const months = [ "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December" ];
 
 window.addEventListener('load', getTime);
 window.addEventListener('load', getDate);
 
-function getTime(){
+function getTime() {
     var date = new Date();
     var hour = date.getHours();
     var minute = date.getMinutes();
     var session = "AM";
 
-    if(hour == 0){
+    if (hour == 0) {
         hour = 12;
-    } else if(hour == 12){
+    } else if (hour == 12) {
         hour = 12;
         session = "PM";
-    } else if(hour > 12){
+    } else if (hour > 12) {
         hour = hour - 12;
         session = "PM";
     }
@@ -28,7 +28,7 @@ function getTime(){
     setTimeout(getTime, 1000);
 }
 
-function getDate(){
+function getDate() {
     var date = new Date();
     var month = date.getMonth();
     var day = date.getDate();
